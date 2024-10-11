5527b5184c5f324404c71f720f2c0e6efef900e3
Jason Kneen <jason.kneen@bouncingfish.com>
2024-10-11 08:37:07 +0100

    WIP

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,6 +25,38 @@ function App({ Component, pageProps }) {
   );
 }
 
+// Debugging function to log language and translation information
+const logLanguageInfo = (pageProps) => {
+  console.log('Current language:', pageProps.lng || 'Not set');
+  console.log('i18n object:', pageProps._nextI18Next);
+  console.log('Namespaces:', pageProps._nextI18Next?.initialI18nStore);
+  console.log('Initial locale:', pageProps._nextI18Next?.initialLocale);
+};
+
+// Add this debugging call to getInitialProps
+App.getInitialProps = async (appContext) => {
+  const { ctx, Component } = appContext;
+  let pageProps = {};
+
+  if (Component && Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  // Determine the language
+  let lng = 'en'; // Default to English
+  if (ctx && ctx.req) {
+    lng = ctx.req.language || 'en';
+  } else if (typeof navigator !== 'undefined') {
+    lng = navigator.language.split('-')[0] || 'en';
+  }
+  console.log('Detected language:', lng);
+
+  // Log additional information about i18n setup
+  logLanguageInfo({ ...pageProps, lng });
+
+  return { pageProps: { ...pageProps, lng } };
+};
+
 App.getInitialProps = async (appContext) => {
   const { ctx, Component } = appContext;
   let pageProps = {};
